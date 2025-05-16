ae23903e71943d2d56f992c1c492d958ed62d3f7
Jake <jfeinbaum@gmail.com>
2025-05-16 14:07:03 -0400

    debug statments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, send_file, Response
+from flask import Flask, render_template, request, Response, jsonify
 from init_db import create_database, database_exists, query_rows, get_database_session
 from model import initialize_phonemes, initialize_edges
 import random
@@ -15,7 +15,7 @@ rows = query_rows(session)
 phonemes = initialize_phonemes()
 edges = initialize_edges()
 
-
+current_answer = None
 
 
 @app.route("/")
@@ -24,7 +24,7 @@ def index():
 
 @app.route("/get-audio")
 def get_audio():
-
+    global current_answer
     last = request.args.get('last')
 
     while True:
@@ -33,14 +33,33 @@ def get_audio():
         if path != last:
             break
 
+    current_answer = choice.answer
+    print('answer set to:', current_answer)
+
     with open(path, 'rb') as f:
         data = f.read()
 
-    # Build response manually with header
     response = Response(data, mimetype='audio/mpeg')
     response.headers['X-Audio-Path'] = path
 
     return response
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    data = request.get_json()
+    seq = data['sequence']
+    print(f'IPA guess: {"".join(seq)}')
+
+    id_seq = ''
+    for char in seq:
+        for p in phonemes:
+            if p.ipa == char:
+                id_seq += p.id
+            elif p.ipa_dot == char:
+                id_seq += p.id_dot
+    print(f'guess: {id_seq} -- answer: {current_answer}')
+
+    return jsonify({'correct': id_seq == current_answer})
 
 if __name__ == "__main__":
     app.run(debug=True)
