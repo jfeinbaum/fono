6e2cfef12870357a47cf2d9e3e9598406e69f5f8
Jake <jfeinbaum@gmail.com>
2025-05-16 12:40:22 -0400

    phoneme model

--- model.py
+++ model.py
@@ -1,53 +1,43 @@
 
 class Phoneme:
-    def __init__(self, id, ipa, dot=False, x=None, y=None):
+    def __init__(self, id, ipa, ipa_dot=None, x=None, y=None):
         self.id = id
         self.ipa = ipa
-        self.dot = dot
-        self.id_dot = self.id.upper() if self.dot else None
+        self.ipa_dot = ipa_dot
+        self.id_dot = self.id.upper() if self.ipa_dot else None
+        self.ipa_dot = ipa_dot
         self.x = x
         self.y = y
 
-
-
-
-
-
-
-
-if __name__ == '__main__':
-
-
-    phonemes = [
-        Phoneme(id='y', ipa='Y', dot=False, x=300, y=100),
-        Phoneme(id='i', ipa='I', dot=False, x=350, y=135),
-        Phoneme(id='=', ipa='⋂', dot=False, x=400, y=175),
-        Phoneme(id='0', ipa='Ǝ', dot=False, x=450, y=215),
-        Phoneme(id='', ipa='Ɐ', dot=False, x=500, y=250),
-        Phoneme(id='9', ipa='A', dot=False, x=500, y=100),
-        Phoneme(id='e', ipa='E', dot=False, x=450, y=135),
-        Phoneme(id='o', ipa='O', dot=False, x=350, y=215),
-        Phoneme(id='u', ipa='U', dot=False, x=300, y=250),
-        Phoneme(id='l', ipa='L', dot=False, x=400, y=100),
-        Phoneme(id='r', ipa='R', dot=False, x=400, y=250),
-        Phoneme(id='P', ipa='P', dot=True, x=600, y=100),
-        Phoneme(id='t', ipa='T', dot=True, x=650, y=100),
-        Phoneme(id='k', ipa='K', dot=True, x=700, y=100),
-        Phoneme(id='b', ipa='B', dot=True, x=600, y=175),
-        Phoneme(id='d', ipa='D', dot=True, x=650, y=175),
-        Phoneme(id='g', ipa='G', dot=True, x=700, y=175),
-        Phoneme(id='m', ipa='M', dot=True, x=600, y=250),
-        Phoneme(id='n', ipa='N', dot=True, x=650, y=250),
-        Phoneme(id='5', ipa='Ŋ', dot=True, x=700, y=250),
-        Phoneme(id='h', ipa='H', dot=False, x=875, y=40),
-        Phoneme(id='f', ipa='F', dot=False, x=800, y=100),
-        Phoneme(id='7', ipa='Φ', dot=False, x=850, y=100),
-        Phoneme(id='s', ipa='S', dot=False, x=900, y=100),
-        Phoneme(id='8', ipa='Ƨ', dot=False, x=950, y=100),
-        Phoneme(id='v', ipa='V', dot=False, x=800, y=175),
-        Phoneme(id='6', ipa='θ', dot=False, x=850, y=175),
-        Phoneme(id='z', ipa='Z', dot=False, x=900, y=175),
-        Phoneme(id='j', ipa='J', dot=False, x=950, y=175)
+def initialize_phonemes():
+    return [
+        Phoneme(id='y', ipa='Y', x=300, y=100),
+        Phoneme(id='i', ipa='I', x=350, y=135),
+        Phoneme(id='=', ipa='⋂', x=400, y=175),
+        Phoneme(id='0', ipa='Ǝ', x=450, y=215),
+        Phoneme(id='9', ipa='Ɐ', x=500, y=250),
+        Phoneme(id='a', ipa='A', x=500, y=100),
+        Phoneme(id='e', ipa='E', x=450, y=135),
+        Phoneme(id='o', ipa='O', x=350, y=215),
+        Phoneme(id='u', ipa='U', x=300, y=250),
+        Phoneme(id='l', ipa='L', x=400, y=100),
+        Phoneme(id='r', ipa='R', x=400, y=250),
+        Phoneme(id='P', ipa='P', ipa_dot='Ṗ', x=600, y=100),
+        Phoneme(id='t', ipa='T', ipa_dot='Ṫ', x=650, y=100),
+        Phoneme(id='k', ipa='K', ipa_dot='K', x=700, y=100),
+        Phoneme(id='b', ipa='B', ipa_dot='Ḃ', x=600, y=175),
+        Phoneme(id='d', ipa='D', ipa_dot='Ḋ', x=650, y=175),
+        Phoneme(id='g', ipa='G', ipa_dot='Ġ̇̇̇', x=700, y=175),
+        Phoneme(id='m', ipa='M', ipa_dot='Ṁ', x=600, y=250),
+        Phoneme(id='n', ipa='N', ipa_dot='Ṅ', x=650, y=250),
+        Phoneme(id='5', ipa='Ŋ', ipa_dot='Ŋ', x=700, y=250),
+        Phoneme(id='h', ipa='H', x=875, y=40),
+        Phoneme(id='f', ipa='F', x=800, y=100),
+        Phoneme(id='7', ipa='Φ', x=850, y=100),
+        Phoneme(id='s', ipa='S', x=900, y=100),
+        Phoneme(id='8', ipa='Ƨ', x=950, y=100),
+        Phoneme(id='v', ipa='V', x=800, y=175),
+        Phoneme(id='6', ipa='θ', x=850, y=175),
+        Phoneme(id='z', ipa='Z', x=900, y=175),
+        Phoneme(id='j', ipa='J', x=950, y=175)
     ]
-
-    pass
